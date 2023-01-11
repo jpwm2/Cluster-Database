b6cf70f90fa81d756e0d13003ad68dcd9b0e5a41
futurejapan <kohsaku.sub2@gmail.com>
2023-01-11 10:50:04 +0900

    withObjver

--- try.py
+++ try.py
@@ -1,18 +1,5 @@
 from Obj import Obj
 
 
-class A:
-    def __init__(self):
-        self.hi = 'HI'
-    
-    def rint(self):
-        print('why')
-        print(self.hi)
-        return 'fuck'
 
-a = A()
-ops = {}
-ops[a] = 'hello'
-
-print(ops[a])
-print(a.rint())
+print(1 or '')
